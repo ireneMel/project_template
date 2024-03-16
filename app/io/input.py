39cc53579f28f74ee71753e0d4c985b3ae573dde
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2024-03-15 23:44:35 -0400

    Add documentation and implementation for IO functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,8 +1,43 @@
+import pandas as pd
+
+
 def read_text():
-    return
+    """
+       Reads user input.
 
-def read_from_file():
-    return
+       Returns:
+           str: The text data.
+    """
+    input("Enter some text: ")
 
-def read_pandas():
-    return
+
+def read_from_file(file_path):
+    """
+    Reads data from a file.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        str: The data read from the file.
+    """
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+def read_pandas(file_path):
+    """
+    Reads data from file using Pandas.
+
+    Args:
+        file_path (str): The path to the file.
+
+    Returns:
+        pandas.DataFrame: The data as a Pandas DataFrame.
+    """
+    try:
+        df = pd.read_csv(file_path)
+        return df
+    except Exception as e:
+        print("Error reading file:", e)
+        return None
--- app/io/output.py
+++ app/io/output.py
@@ -1,5 +1,18 @@
-def write_text():
+def write_text(text):
+    """
+    Writes text data to a file.
+
+    Args:
+        text (str): The text data to be written to the file.
+    """
     return
 
-def write_into_file():
+def write_into_file(data, file_path):
+    """
+       Writes data into a file.
+
+       Args:
+           data: The data to be written into the file.
+           file_path (str): The path to the file.
+    """
     return
