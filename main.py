880e31e2c8e8e005d380ebc87e3efd5a936c7b9a
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2024-03-15 23:44:51 -0400

    Add usage of IO functions

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import sys
+
+from app.io.input import read_text, read_from_file, read_pandas
+
+
 def main():
-    pass
+    sys.stdout = open("test_files/outputs/output.txt", "w")
+
+    text_data = read_text()
+    print("Text data: ", text_data)
+
+    file_data = read_from_file("test_files/test_file.txt")
+    print("Data from file:\n", file_data)
+
+    pandas_data = read_pandas("test_files/test_file_2.csv")
+    print("Pandas data: ", pandas_data)
+
+    sys.stdout.close()
+
 
 if __name__ == "__main__":
     main()
