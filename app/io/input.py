b0a2f03722a99ae5230f42a8a872b4c27a51f9ad
Iryna Melnyk <iryna2.melnyk@ukma.edu.ua>
2024-03-15 23:25:31 -0400

    Add IO functions declaration

---
+++ app/io/input.py
@@ -0,0 +1,8 @@
+def read_text():
+    return
+
+def read_from_file():
+    return
+
+def read_pandas():
+    return
---
+++ app/io/output.py
@@ -0,0 +1,5 @@
+def write_text():
+    return
+
+def write_into_file():
+    return
